6c0bf98e68e54cf1cfb95046f0d595c1391a8db3
Arthur-Bottemanne <arthurbottemanne@gmail.com>
2024-08-29 16:26:12 +0200

    feat: add simple router

--- public/index.php
+++ public/index.php
@@ -6,5 +6,15 @@ define('BASE_DIR', dirname( __FILE__ ).'/..');
 define('SOURCE_DIR', BASE_DIR.'/src');
 define('VIEW_DIR', SOURCE_DIR.'/views');
 
+$request = $_SERVER['REQUEST_URI'];
 
-require VIEW_DIR.'/home.php';
+switch ($request) {
+    case '':
+    case '/':
+        require VIEW_DIR.'/home.php';
+        break;
+
+    default:
+        http_response_code(404);
+        require VIEW_DIR.'/404.php';
+}
