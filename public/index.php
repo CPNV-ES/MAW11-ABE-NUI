18e716674580c4b597176cf0361f315be2fb74f5
Nazmi Uksmajli <pg43fka@eduvaud.ch>
2024-10-02 10:53:09 +0200

    Frontend create an exercise

--- public/index.php
+++ public/index.php
@@ -19,7 +19,12 @@ switch ($request) {
     case '/':
         include VIEW_DIR . '/home.php';
         break;
-
+    case '/exercises/new':
+        include VIEW_DIR . '/create.php';
+        break;
+    case '/exercises/answering':
+        include VIEW_DIR . '/Take.php';
+        break;
     default:
         http_response_code(404);
         include VIEW_DIR . '/404.php';
