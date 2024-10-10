d1c82c6be762c5f407d33bcd49cdaac6c274ed88
Arthur-Bottemanne <arthurbottemanne@gmail.com>
2024-10-10 11:09:23 +0200

    fix: modified frontend to resemble looper website

--- src/views/Fields.php
+++ src/views/Fields.php
@@ -1,49 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New Exercise</title>
-    <link rel="stylesheet" href="Fields.css">
-</head>
-<body>
-    <header class="header">
-        <div class="header-content">
-            <img src="logo.png" alt="Exercise Looper Logo" class="logo">
-            <span>New exercise</span>
-        </div>
-    </header>
+<?php
 
-    <div class="container">
-        <div class="left-panel">
-            <h2>Fields</h2>
-            <table>
+//initialize page variables
+$title = "ExerciseLooper";
+$style = '<link rel="stylesheet" href="/css/Fields.css">';
+
+ob_start();
+?>
+
+
+<header class="heading managing">
+    <section class="container">
+        <a href="/"><img src="/img/logo.png" /></a>
+        <span class="exercise-label">Exercise: <a href="/exercises/141/fields">teetatea</a></span>
+    </section>
+</header>
+
+<main class="container">
+    <div class="row">
+        <section class="column">
+            <h1>Fields</h1>
+            <table class="records">
                 <thead>
                     <tr>
                         <th>Label</th>
                         <th>Value kind</th>
+                        <th></th>
                     </tr>
                 </thead>
+
                 <tbody>
                 </tbody>
             </table>
-            <button class="btn purple">Complete and be ready for answers</button>
-        </div>
 
-        <div class="right-panel">
-            <h2>New Field</h2>
-            <form>
-                <label for="label">Label</label>
-                <input type="text" id="label" name="label">
+            <a data-confirm="Are you sure? You won&#39;t be able to further edit this exercise" class="button" rel="nofollow" data-method="put" href="/exercises/141?exercise%5Bstatus%5D=answering"><i class="fa fa-comment"></i> Complete and be ready for answers</a>
 
-                <label for="value-kind">Value kind</label>
-                <select id="value-kind" name="value-kind">
-                    <option value="single-line">Single line text</option>
-                </select>
+        </section>
+        <section class="column">
+            <h1>New Field</h1>
+            <form action="/exercises/141/fields" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="1TIOsTub8Dmv8NPGhgCklwuZcN9SUadvrbBzqmQDPuSu0xZMrh0/uP616LsMpQiva7F3wc6BPHFjtKIQqD2G2g==" />
 
-                <button type="submit" class="btn purple">Create Field</button>
+                <div class="field">
+                    <label for="field_label">Label</label>
+                    <input type="text" name="field[label]" id="field_label" />
+                </div>
+
+                <div class="field">
+                    <label for="field_value_kind">Value kind</label>
+                    <select name="field[value_kind]" id="field_value_kind">
+                        <option selected="selected" value="single_line">Single line text</option>
+                        <option value="single_line_list">List of single lines</option>
+                        <option value="multi_line">Multi-line text</option>
+                    </select>
+                </div>
+
+                <div class="actions">
+                    <input type="submit" name="commit" value="Create Field" data-disable-with="Create Field" />
+                </div>
             </form>
-        </div>
+        </section>
     </div>
-</body>
-</html>
+</main>
+
+<?php
+
+$content = ob_get_clean();
+
+require VIEW_DIR . "/layout.php";
